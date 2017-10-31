4a9eaa715e2064581958665fd21b2759aeef0a41
Ayush Sharma <ayush.sharma@innovaccer.com>
2017-10-31 11:58:55 +0530

    driver.wait integrated

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    imageClass: 'recCard__img StretchedBox',
+}
+
+module.exports.config = config
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+const request = require('request');
 const {Builder, By, Key, until} = require('selenium-webdriver');
+
+// configuration
+const config = require('./config');
+
+console.log(config);
 
 let driver = new Builder()
     .forBrowser('chrome')
@@ -6,15 +13,53 @@ let driver = new Builder()
 
 const swipeRight = (i) => {
     console.log(i);
-    driver.findElement(By.tagName("body")).sendKeys("webdriver", Key.ARROW_RIGHT);
+    return driver.findElement(By.tagName("body")).sendKeys("webdriver", Key.ARROW_RIGHT);
 }
 
+const findImage = () => {
+  return driver
+    .wait(
+      until.elementLocated(By.css("div.recCard__img.StretchedBox")), 6000000)
+    .then(el => {
+      return el;
+    });
+};
+
+function downloadFile(localPath, name, url) {
+  return new Promise(function(reject, resolve) {
+    console.log(">>>>>>>>>>>>>>>>>>.");
+    console.log(url, localPath, name);
+    const fileName = `${localPath}/${name}.jpg`;
+    if (!fs.existsSync(localPath)) {
+      fs.mkdirSync(localPath);
+    }
+
+    request(url)
+      .on("error", reject("error"))
+      .pipe(fs.createWriteStream(fileName));
+    on("finish", () => {
+      resolve("true");
+    });
+  });
+};
+
+
 driver.get('https://tinder.com');
-driver.sleep(50000); 
+// driver.sleep(20000); 
 // so in these 35 seconds this time just login using fb manually.
 // I was feeling so lazy to write automation for this.
-[...Array(1000)].forEach((e,i) => {
-    driver.sleep(1000).then(() => swipeRight(i));
+[...Array(10000)].forEach((value, index) => {
+    driver
+      .then(() => findImage())
+      .then(elem => elem.getAttribute("style"))
+      .then(rawLink => {
+        const downloadUrl = rawLink.split("");
+        downloadUrl.splice(0, 23);
+        downloadUrl.splice(-3);
+        return downloadUrl.join("");
+      })
+      .then(url => console.log(url))
+      .then(() => swipeRight(index));
 });
 
 driver.quit();
