8a761966ab2915b4d620515df26fe5bce3c676fb
Ayush Sharma <ayush.sharma@innovaccer.com>
2017-11-20 16:08:00 +0530

    Reload button tests
    Reload click button

--- test/web/reload.spec.js
+++ test/web/reload.spec.js
@@ -1,6 +1,28 @@
 /**
- * Test case
+ * Reload component
+ * @author ayusharma
  */
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+
+import { shallow } from 'enzyme';
+import Reload from '../../app/web/components/Reload';
+import React from 'react';
+import { wrap } from 'module';
+
+describe('<Reload /> UI comonent', () => {
+  it('should render a button', () => {
+    const wrapper = shallow(<Reload />);
+    expect(wrapper.find('button').length).toBe(1);
+  });
+
+  it('button text should be relaod', () => {
+    const wrapper = shallow(<Reload />);
+    expect(wrapper.find('button').text()).toMatch('Reload');
+  });
+
+  it('invokes click on reload button', () => {
+    const _click = jest.fn();
+    const wrapper = shallow(<Reload onClick={_click} />);
+    wrapper.find('button').simulate('click');
+    expect(_click).toBeCalled();
+  });
 });
